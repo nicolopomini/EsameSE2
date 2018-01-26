90dc75539c298f80145c631a0cfc57cd48071b86
Nicolò Pomini <nicolo.pomini@studenti.unitn.it>
2018-01-26 10:58:28 +0100

    check post dome

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express'),
     bodyParser = require('body-parser');
-
+const check = require('./checker');
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,7 +11,16 @@ app.set('port', (process.env.PORT || 5000));
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
 app.get('/count',function (req, res) {
     res.json({count: 5})
-})
+});
+app.post('/check', (req, res) => {
+	var url = req.body.url;
+	var par = req.body.invocationParameters;
+	var exdata = req.body.expectedResultData;
+	var extstatus = req.body.expectedResultStatus;
+	check(url, par, exdata, extstatus).then(risp => {
+		res.json(risp);
+	});
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
